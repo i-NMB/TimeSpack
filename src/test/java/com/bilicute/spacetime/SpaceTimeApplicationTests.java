7478cd5cc6f20fee01ef547aa2f1738628d5c160
iNMB <inmb@foxmail.com>
2024-04-13 19:42:53 +0800

    初始化程序

---
+++ src/test/java/com/bilicute/spacetime/SpaceTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilicute.spacetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
