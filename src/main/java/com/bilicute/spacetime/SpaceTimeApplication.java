3622230422ee95cf4e6592620ea1e473362cf8bc
mjlyb <14299221+mjlyb@user.noreply.gitee.com>
2024-04-15 10:23:46 +0800

    修改注释-测试2

--- src/main/java/com/bilicute/spacetime/SpaceTimeApplication.java
+++ src/main/java/com/bilicute/spacetime/SpaceTimeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpaceTimeApplication {
-//测试
-    //duoxie
+//测试123
     public static void main(String[] args) {
         SpringApplication.run(SpaceTimeApplication.class, args);
     }
