a0351356d1d2a7cd9e85c4432f83b2ccf9741fe7
iNMB <inmb@foxmail.com>
2024-04-16 10:28:53 +0800

    更新远程数据库，并为关键信息进行加密

--- src/main/java/com/bilicute/spacetime/mapper/UserMapper.java
+++ src/main/java/com/bilicute/spacetime/mapper/UserMapper.java
@@ -2,8 +2,9 @@ package com.bilicute.spacetime.mapper;
 
 import com.bilicute.spacetime.pojo.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
+@Mapper
 public interface UserMapper {
     //根据用户名查询用户
     @Select("select * from user where username=#{username}")
--- src/test/java/com/bilicute/spacetime/SpaceTimeApplicationTests.java
+++ src/test/java/com/bilicute/spacetime/SpaceTimeApplicationTests.java
@@ -1,13 +1,38 @@
 package com.bilicute.spacetime;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpaceTimeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
+    public static String stringEncryptor(String secretKey, String message, boolean isEncrypt) {
+        PooledPBEStringEncryptor pooledPBEStringEncryptor = new PooledPBEStringEncryptor();
+        pooledPBEStringEncryptor.setConfig(getSimpleStringPBEConfig(secretKey));
+        String result = isEncrypt ? pooledPBEStringEncryptor.encrypt(message) : pooledPBEStringEncryptor.decrypt(message);
+        return result;
+    }
+    private static SimpleStringPBEConfig getSimpleStringPBEConfig(String secretKey) {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(secretKey);
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        return config;
+    }
+    @Test
+    public void contextLoads() {
+        String message = "";
+        String password = "";
+        //一个同样的密码和秘钥，每次执行加密，密文都是不一样的。但是解密是没问题的。
+        String jasyptEncrypt = stringEncryptor(password, message, true);
+        System.out.println(jasyptEncrypt);
+    }
 }
