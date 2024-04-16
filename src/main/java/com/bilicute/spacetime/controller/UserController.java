5f65f878c68da82ca107cdaf289d6180b4248750
禹婷婷 <14303529+yu-tingtingt@user.noreply.gitee.com>
2024-04-16 17:41:08 +0800

    注册登录模块完成

--- src/main/java/com/bilicute/spacetime/controller/UserController.java
+++ src/main/java/com/bilicute/spacetime/controller/UserController.java
@@ -3,6 +3,7 @@ package com.bilicute.spacetime.controller;
 import com.bilicute.spacetime.pojo.Result;
 import com.bilicute.spacetime.pojo.User;
 import com.bilicute.spacetime.service.UserService;
+import com.bilicute.spacetime.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,19 @@ public class UserController {
 
     }else{
         return Result.error("参数不合法"); }
+    }
+    public Result<String> login(@Pattern(regexp ="^\\S{5,16}$") String username, @Pattern(regexp ="^\\S{5,16}$")String password){
+    //根据用户名查询用户
+        User loginUser=userService.findByUserName(username );
+    //判断该用户是否存在
+        if(loginUser==null){
+            return Result.error("用户名错误");
+        }
+        //判断密码是否正确loginguser对象中的password是密文
+        if (Md5Util.getMD5String(password).equals(loginUser.getPassword()) ){
+            //登录成功
+            return Result.success("jwt token令牌..");
+        }
+        return Result.error("密码错误");
     }
 }
--- src/main/java/com/bilicute/spacetime/pojo/User.java
+++ src/main/java/com/bilicute/spacetime/pojo/User.java
@@ -5,13 +5,13 @@ import lombok.Data;
 import java.time.LocalDateTime;
 @Data
 public class User {
-    private Integer createUser;//主键ID
+    private Integer create_user;//主键ID
     private String username;//用户名
     private String password;//密码
     private String nickname;//昵称
     private String email;//邮箱
-    private String userPic;//用户头像地址
-    private String creatTime;//创建时间
-    private String updateTime;//更新时间
+    private String user_pic;//用户头像地址
+    private String creat_time;//创建时间
+    private String update_time;//更新时间
     private String identity;//身份
 }
