20a96c01e4a2c511568521f3ace7d8f24bb39aa8
禹婷婷 <14303529+yu-tingtingt@user.noreply.gitee.com>
2024-04-16 17:11:25 +0800

    用户注册完成

--- src/main/java/com/bilicute/spacetime/controller/UserController.java
+++ src/main/java/com/bilicute/spacetime/controller/UserController.java
@@ -3,6 +3,7 @@ package com.bilicute.spacetime.controller;
 import com.bilicute.spacetime.pojo.Result;
 import com.bilicute.spacetime.pojo.User;
 import com.bilicute.spacetime.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -19,7 +20,13 @@ public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public Result register(String username, String password){
+    public Result register(@Pattern(regexp ="^\\S{5,16}$") String username, @Pattern(regexp ="^\\S{5,16}$")String password) {
+        if (username != null && username.length() >= 5 && username.length() <= 16 &&
+                password != null && password.length() >= 5 && password.length() <= 16)
+        {
+
+
+
         //查询用户
         User u = userService.findByUserName(username);
         if (u!=null){
@@ -32,5 +39,7 @@ public class UserController {
         userService.register(username,password);
         return Result.success();
 
+    }else{
+        return Result.error("参数不合法"); }
     }
 }
--- src/main/java/com/bilicute/spacetime/pojo/User.java
+++ src/main/java/com/bilicute/spacetime/pojo/User.java
@@ -1,16 +1,6 @@
 package com.bilicute.spacetime.pojo;
 
 
-import lombok.Data;
-import java.time.LocalDateTime;
-@Data
+
 public class User {
-    private Integer id;//主键ID
-    private String username;//用户名
-    private String password;//密码
-    private String nickname;//昵称
-    private String email;//邮箱
-    private String userPic;//用户头像地址
-    private String creatTime;//创建时间
-    private String updateTime;//更新时间
 }
