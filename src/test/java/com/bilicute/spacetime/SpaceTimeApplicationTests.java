516933b884fa3ce2e3c588febc4272278792a303
iNMB <inmb@foxmail.com>
2024-07-29 17:56:35 +0800

    完善ci.yml工作流程文件,添加指向文件测试6

--- src/test/java/com/bilicute/spacetime/SpaceTimeApplicationTests.java
+++ src/test/java/com/bilicute/spacetime/SpaceTimeApplicationTests.java
@@ -2,10 +2,6 @@ package com.bilicute.spacetime;
 
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.junit.jupiter.api.Test;
-
-import java.util.Objects;
-import java.util.Scanner;
 
 //@SpringBootTest
 class SpaceTimeApplicationTests {
@@ -30,25 +26,25 @@ class SpaceTimeApplicationTests {
         return config;
     }
 
-    @Test
-    public void contextLoads() {
-        System.out.print("请输入密钥:");
-        Scanner p = new Scanner(System.in);
-        String password = p.next();
-        System.out.print("是否为加密（输入true为加密，输入false为解密）:");
-        Scanner enc = new Scanner(System.in);
-        boolean isEncrypt = Boolean.parseBoolean(enc.next());
-        while (true) {
-            System.out.println("请输入要加密/解密的文字，输入exit退出");
-            Scanner s = new Scanner(System.in);
-            String message = s.next();
-            if (Objects.equals(message, "exit")) {
-                break;
-            }
-            //一个同样的密码和秘钥，每次执行加密，密文都是不一样的。但是解密是没问题的。
-            //若解密时报错，请确保密钥正确
-            String jasyptEncrypt = stringEncryptor(password, message, isEncrypt);
-            System.out.println(jasyptEncrypt);
-        }
-    }
+//    @Test
+//    public void contextLoads() {
+//        System.out.print("请输入密钥:");
+//        Scanner p = new Scanner(System.in);
+//        String password = p.next();
+//        System.out.print("是否为加密（输入true为加密，输入false为解密）:");
+//        Scanner enc = new Scanner(System.in);
+//        boolean isEncrypt = Boolean.parseBoolean(enc.next());
+//        while (true) {
+//            System.out.println("请输入要加密/解密的文字，输入exit退出");
+//            Scanner s = new Scanner(System.in);
+//            String message = s.next();
+//            if (Objects.equals(message, "exit")) {
+//                break;
+//            }
+//            //一个同样的密码和秘钥，每次执行加密，密文都是不一样的。但是解密是没问题的。
+//            //若解密时报错，请确保密钥正确
+//            String jasyptEncrypt = stringEncryptor(password, message, isEncrypt);
+//            System.out.println(jasyptEncrypt);
+//        }
+//    }
 }
